da68fbb560ca7ec5216ba7cc74d8aad8cdf363d7
hermelen <Hermelen>
2018-06-13 12:30:07 +0200

    well done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var shift = 3;
+var shift = 300;
 // var text = window.prompt("tapper votre texte");
 var text = "alexia";
 
@@ -7,7 +7,16 @@ var result = "";
 if (text != null) {
   for (var i = 0; i < text.length; i++) {
     letterIndex = alpha.indexOf(text[i]);
-    result += alpha[letterIndex+shift];
+    index = letterIndex + shift;
+    index %= 26;
+    // if (letterIndex + shift >= 26) {
+    //   index = letterIndex + shift - 26;
+    //   console.log(index);
+    // } else {
+    //   index = letterIndex + shift;
+    //   console.log(index);
+    // }
+    result += alpha[index];
   }
 }
 
