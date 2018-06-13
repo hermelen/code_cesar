5f54262eab5b9430af3151217116159304e196d7
hermelen <Hermelen>
2018-06-13 11:14:53 +0200

    démarrage

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var text = window.prompt("tapper votre texte");
+
+if (text != null) {
+  for (var i = 0; i < alpha.length; i++) {
+    letter = text.indexOf(alpha[i]);
+  }
+}
+// console.log(alpha[10]);
